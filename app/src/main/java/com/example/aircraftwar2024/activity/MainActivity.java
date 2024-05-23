96443685558fbc54109d4cb67605c59831f42660
Chentao-Xu <ct.xu@outlook.com>
2024-05-23 18:10:01 +0800

    first commit

---
+++ app/src/main/java/com/example/aircraftwar2024/activity/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.aircraftwar2024.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+import com.example.aircraftwar2024.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private boolean isMusicOn = false;
+    private RadioGroup musicRadioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button singlePlayerButton = findViewById(R.id.singlePlayerButton);
+        musicRadioGroup = findViewById(R.id.musicRadioGroup);
+
+        singlePlayerButton.setOnClickListener(this);
+
+        musicRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.musicOnRadioButton) {
+                isMusicOn = true;
+            }
+            else if (checkedId == R.id.musicOffRadioButton){
+                    isMusicOn = false;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity.this, OfflineActivity.class);
+        intent.putExtra("isMusicOn",isMusicOn);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/aircraftwar2024/activity/OfflineActivity.java
@@ -0,0 +1,49 @@
+package com.example.aircraftwar2024.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.aircraftwar2024.R;
+
+public class OfflineActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private boolean isMusicOn;
+    private int gameType;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline);
+
+        Button easyModButton = findViewById(R.id.EasyModButton);
+        Button normalModButton = findViewById(R.id.NormalModButton);
+        Button hardModButton = findViewById(R.id.HardModButton);
+
+        easyModButton.setOnClickListener(this);
+        normalModButton.setOnClickListener(this);
+        hardModButton.setOnClickListener(this);
+
+        this.isMusicOn = getIntent().getBooleanExtra("isMusicOn",false);
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.EasyModButton) {
+            gameType = 0;
+            Log.i("Button",""+gameType);
+        }else if (v.getId() == R.id.NormalModButton) {
+            gameType = 1;
+        } else if (v.getId() == R.id.HardModButton) {
+            gameType = 2;
+        }
+        Intent intent = new Intent(OfflineActivity.this, GameActivity.class);
+        intent.putExtra("isMusicOn",isMusicOn);
+        intent.putExtra("gameType",gameType);
+        startActivity(intent);
+    }
+}
